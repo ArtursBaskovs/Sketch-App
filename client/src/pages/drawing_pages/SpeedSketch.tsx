7e2ba1541f2406a1a54491349d6fa061b9f0fbda
unknown <12arturb34@gmail.com>
2025-11-01 04:11:36 +0200

    Reworking canvas component to achieve smoother lines

--- client/src/pages/drawing_pages/SpeedSketch.tsx
+++ client/src/pages/drawing_pages/SpeedSketch.tsx
@@ -1,5 +1,6 @@
-import { DrawCanva } from "../../components/ui/Drawing/DrawCanva"
-import { DrawingToolsPanel } from "../../components/ui/Drawing/DrawingToolsPanel"
+import { DrawCanva } from "../../components/ui/Drawing/Canvas/DrawCanva"
+import { DrawingToolsPanel } from "../../components/ui/Drawing/Brush_tools/DrawingToolsPanel"
+import { DrawingCanva } from "../../components/ui/Drawing/Canvas/DrawingCanva"
 
 export const SpeedSketch: React.FC = () => {
 
@@ -8,7 +9,7 @@ export const SpeedSketch: React.FC = () => {
         <h1>Speed Sketch</h1>
         <div className="flex flex-row z-1 justify-evenly w-full">
             <DrawingToolsPanel />
-            <DrawCanva/>
+            <DrawCanva />
         </div>
 
         </>
